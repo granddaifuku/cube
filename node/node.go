93636c009acb3a08f093e281e5f0a4b01d203300
granddaifuku <daifukuformac@gmail.com>
2024-08-07 01:54:23 +0900

    Node skeleton

---
+++ node/node.go
@@ -0,0 +1,15 @@
+package node
+
+// Represents any machines in the cluster
+
+type Node struct {
+	Name            string
+	Ip              string
+	Cores           int
+	Memory          int
+	MemoryAllocated int
+	Disk            int
+	DiskAllocated   int
+	Role            string
+	TaskCount       int
+}
