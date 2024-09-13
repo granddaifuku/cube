cf53b748ef85d13773d8b8ec3686f0d96ac674a9
Yudai Fukushima <49578068+granddaifuku@users.noreply.github.com>
2024-09-13 22:15:50 +0900

    Part 3 Chapter 8: An API for the Manager (#7)
    * Implement router
    
    * Add some methods
    
    * Test

--- manager/manager.go
+++ manager/manager.go
@@ -8,6 +8,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/golang-collections/collections/queue"
 	"github.com/google/uuid"
@@ -114,7 +115,7 @@ func (m *Manager) SendWork() {
 	}
 }
 
-func (m *Manager) UpdateTasks() {
+func (m *Manager) updateTasks() {
 	for _, worker := range m.Workers {
 		log.Printf("Checking worker %v for task updates", worker)
 		url := fmt.Sprintf("http://%s/tasks", worker)
@@ -154,6 +155,34 @@ func (m *Manager) UpdateTasks() {
 	}
 }
 
+func (m *Manager) UpdateTasks() {
+	for {
+		log.Println("Checking for task updates from workers")
+		m.updateTasks()
+		log.Println("Task updates completed")
+		log.Println("Sleeping for 15 secs")
+		time.Sleep(15 * time.Second)
+	}
+}
+
+func (m *Manager) ProcessTasks() {
+	for {
+		log.Println("Processing any tasks in the queue")
+		m.SendWork()
+		log.Println("Sleeping for 10 secs")
+		time.Sleep(10 * time.Second)
+	}
+}
+
 func (m *Manager) AddTask(te task.TaskEvent) {
 	m.Pending.Enqueue(te)
+}
+
+func (m *Manager) GetTasks() []*task.Task {
+	tasks := []*task.Task{}
+	for _, t := range m.TaskDb {
+		tasks = append(tasks, t)
+	}
+
+	return tasks
 }
