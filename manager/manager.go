a0687632d23bd29832fec7f0eea34767a1376946
granddaifuku <daifukuformac@gmail.com>
2024-09-13 14:50:37 +0900

    Create New manager function

--- manager/manager.go
+++ manager/manager.go
@@ -27,6 +27,25 @@ type Manager struct {
 	TaskWorkerMap map[uuid.UUID]string
 }
 
+func New(workers []string) *Manager {
+	taskDb := make(map[uuid.UUID]*task.Task)
+	eventDb := make(map[uuid.UUID]*task.TaskEvent)
+	workerTaskMap := make(map[string][]uuid.UUID)
+	taskWorkerMap := make(map[uuid.UUID]string)
+	for worker := range workers {
+		workerTaskMap[workers[worker]] = []uuid.UUID{}
+	}
+
+	return &Manager{
+		Pending:       *queue.New(),
+		Workers:       workers,
+		TaskDb:        taskDb,
+		EventDb:       eventDb,
+		WorkerTaskMap: workerTaskMap,
+		TaskWorkerMap: taskWorkerMap,
+	}
+}
+
 // Serve as a scheduler
 func (m *Manager) SelectWorker() string {
 	// Round-robin
